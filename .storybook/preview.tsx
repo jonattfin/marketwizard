a9ba4f5985d94dd71d9c33d1a41bd8234bf1818b
jonattfin <jonattfin@outlook.com>
2026-07-30 00:10:15 +0300

    fixed the stories / tests

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,18 @@
-import type { Preview } from '@storybook/nextjs-vite'
+import {mswLoader} from 'msw-storybook-addon/csf3'
+import {Preview} from "@storybook/nextjs-vite";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+// @ts-ignore
+import {Provider} from "@/components/ui/provider";
+import {Box, Theme} from "@chakra-ui/react";
+
+const queryClient = new QueryClient()
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -14,8 +21,25 @@ const preview: Preview = {
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
       test: 'todo'
-    }
+    },
   },
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader()],
+  decorators: [
+    (Story, {}) => {
+      return (
+        <Provider>
+          <QueryClientProvider client={queryClient}>
+            <Theme appearance={"light"}>
+              <Box padding="50px">
+                <Story/>
+              </Box>
+            </Theme>
+          </QueryClientProvider>
+        </Provider>
+      )
+    },
+  ]
 };
 
 export default preview;
